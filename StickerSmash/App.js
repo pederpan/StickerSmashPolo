9fac485d281c5440d9bd10a6f4ee140306212f36
pederpan <144904600+pederpan@users.noreply.github.com>
2024-09-20 10:40:57 -0300

    Add files via upload

---
+++ StickerSmash/App.js
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+
+ import ImageViewer from './components/ImageViewer'; 
+
+const PlaceholderImage = require('./assets/images/background-image.png');
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+});
+
+// continuar no passo 6 do Construir uma tela... a gente só criou o Button.js mas não mudou o App.js
---
+++ StickerSmash/README.md
@@ -0,0 +1,2 @@
+npx expo install react-dom react-native-web @expo/metro-runtime  
+npx expo start  
